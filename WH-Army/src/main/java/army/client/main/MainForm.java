d073c611e01a0d55672a4b7bb8aa15dbcf39d541
Tau <tau@tau-UL30VT.(none)>
2012-01-06 16:07:13 +0600

    пыщ

---
+++ WH-Army/src/main/java/army/client/main/MainForm.java
@@ -0,0 +1,20 @@
+package army.client.main;
+
+import com.extjs.gxt.ui.client.widget.Composite;
+import com.extjs.gxt.ui.client.widget.form.LabelField;
+import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.google.gwt.uibinder.client.UiField;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tau
+ * Date: 03.01.12
+ * Time: 15:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainForm extends Composite {
+
+    @UiField
+    LabelField field;
+
+}
