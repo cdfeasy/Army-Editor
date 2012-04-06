eae2325d4f8aced5dd2b95cfe894adf750683168
AnnaTau <branntau@gmail.com>
2012-04-06 23:58:11 +0600

    добавление новых зависимостей

--- WH-Army-web/src/main/java/army/client/secondForm/SecondForm.java
+++ WH-Army-web/src/main/java/army/client/secondForm/SecondForm.java
@@ -1,8 +1,12 @@
 package army.client.secondForm;
 
+import com.extjs.gxt.ui.client.widget.VerticalPanel;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -18,9 +22,13 @@ public class SecondForm extends Composite {
 
     private static SecondFormUiBinder uiBinder = GWT.create(SecondFormUiBinder.class);
 
+    @UiField
+    ListBox listBox;
+
     public SecondForm() {
         super();
         initWidget(uiBinder.createAndBindUi(this));
-
+        listBox.addItem("пыщ");
+//        RootPanel.get().add(new VerticalPanel());
     }
 }
