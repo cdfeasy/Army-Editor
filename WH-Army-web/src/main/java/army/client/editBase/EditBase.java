847e60db78eee545806eaf4dffe9f9d16e94c553
AnnaTau <branntau@gmail.com>
2012-05-17 14:25:11 +0600

    косяки с hibernate

--- WH-Army-web/src/main/java/army/client/editBase/EditBase.java
+++ WH-Army-web/src/main/java/army/client/editBase/EditBase.java
@@ -1,20 +1,9 @@
 package army.client.editBase;
 
-import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.ui.IsWidget;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.Widget;
-//import com.sencha.gxt.examples.resources.client.TestData;
-//import com.sencha.gxt.explorer.client.model.Example.Detail;
-import com.sencha.gxt.widget.core.client.Composite;
-import com.sencha.gxt.widget.core.client.TabItemConfig;
-import com.sencha.gxt.widget.core.client.TabPanel;
-import com.sencha.gxt.widget.core.client.info.Info;
+import com.google.gwt.user.client.ui.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,27 +13,18 @@ import com.sencha.gxt.widget.core.client.info.Info;
  * To change this template use File | Settings | File Templates.
  */
 public class EditBase extends Composite {
-    interface MyUiBinder extends UiBinder<Widget, EditBase> {
+    interface SecondFormUiBinder extends UiBinder<Widget, EditBase> {
     }
 
-    private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+    private static SecondFormUiBinder uiBinder = GWT.create(SecondFormUiBinder.class);
 
-//    @UiField(provided = true)
-//    String txt = TestData.DUMMY_TEXT_SHORT;
+    @UiField
+    ListBox listBox;
 
-    public Widget asWidget() {
-        return uiBinder.createAndBindUi(this);
-    }
-
-    public void onModuleLoad() {
-        RootPanel.get().add(asWidget());
-    }
-
-    @UiHandler(value = {"folder", "panel"})
-    void onSelection(SelectionEvent<Widget> event) {
-        TabPanel panel = (TabPanel) event.getSource();
-        Widget w = event.getSelectedItem();
-        TabItemConfig config = panel.getConfig(w);
-        Info.display("Message", "'" + config.getText() + "' Selected");
+    public EditBase() {
+        super();
+        initWidget(uiBinder.createAndBindUi(this));
+        listBox.addItem("пыщ");
+//        RootPanel.get().add(new VerticalPanel());
     }
 }
