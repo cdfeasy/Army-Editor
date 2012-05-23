22a656d37cc23fa3c392286b96a4d8324e5d9804
cdf.easy <cdf.easy@gmail.com>
2012-05-23 20:57:24 +0400

    нерабочий коммит

--- web-admin/src/main/java/com/webadmin/client/admin.java
+++ web-admin/src/main/java/com/webadmin/client/admin.java
@@ -3,19 +3,27 @@ package com.webadmin.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
+
+
+
+
 
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class admin implements EntryPoint {
+public class admin implements IsWidget,EntryPoint {
 
     interface MyUiBinder extends UiBinder<Widget, admin> {
     }
 
     private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+
+	
 
     /**
      * This is the entry point method.
