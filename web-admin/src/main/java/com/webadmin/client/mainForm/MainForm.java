f6e58bc4c2bf627c0101af0934ff1ecaf1b0e857
AnnaTau <branntau@gmail.com>
2012-05-23 21:28:07 +0600

    gxt не видит контейнеры

---
+++ web-admin/src/main/java/com/webadmin/client/mainForm/MainForm.java
@@ -0,0 +1,11 @@
+package com.webadmin.client.mainForm;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tau
+ * Date: 22.05.12
+ * Time: 17:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainForm {
+}
