cacc1641cd1285987e9f1eba3e17e573b5494fb4
AnnaTau <branntau@gmail.com>
2012-05-28 15:23:23 +0600

    оно наконец заработало

--- web-admin/src/main/java/com/webadmin/client/admin.java
+++ web-admin/src/main/java/com/webadmin/client/admin.java
@@ -2,11 +2,16 @@ package com.webadmin.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.TabItemConfig;
+import com.sencha.gxt.widget.core.client.TabPanel;
+import com.sencha.gxt.widget.core.client.info.Info;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -18,6 +23,8 @@ public class admin implements EntryPoint {
 
     private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
 
+    @UiField(provided = true)
+    String txt = "ваааааа";
     /**
      * This is the entry point method.
      */
@@ -27,5 +34,13 @@ public class admin implements EntryPoint {
 
     public void onModuleLoad() {
         RootPanel.get().add(asWidget());
+    }
+
+    @UiHandler(value = {"folder", "panel"})
+    void onSelection(SelectionEvent<Widget> event) {
+        TabPanel panel = (TabPanel) event.getSource();
+        Widget w = event.getSelectedItem();
+        TabItemConfig config = panel.getConfig(w);
+        Info.display("Message", "'" + config.getText() + "' Selected");
     }
 }
