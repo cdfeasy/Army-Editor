d3c954b03f33a54423183e681fcf075b8736b124
cdf.easy <cdf.easy@gmail.com>
2012-07-18 20:27:51 +0400

    маршаллинг

--- armyeditor-entrys/src/main/java/com/armyeditor/entrys/Codex.java
+++ armyeditor-entrys/src/main/java/com/armyeditor/entrys/Codex.java
@@ -62,24 +62,55 @@ public class Codex implements java.io.Serializable  {
     public void setSquads(List<SquadBase> squads) {
         this.squads = squads;
     }
+	
+	public void parseUnits(String offset, StringBuilder smain,UnitBase unit,Set<Option> lstopt,Set<Weapon> lstweap,Set<UnitBase> lstunits,Set<ItemBase> lstitems){
+		lstunits.add(unit);
+		smain.append(offset).append("unitId=").append(unit.getId()).append("\n");
+		smain.append(offset).append("options:[");
+		for(Option opt:unit.getOptions()){
+			smain.append(opt.getId()).append(";");
+			lstopt.add(opt);
+		}
+		smain.append("]").append("\n");
+		smain.append(offset).append("Weapon:[");
+		for(Weapon weap:unit.getWeapons()){
+			smain.append(weap.getWeapon().getId()).append(";");
+			lstweap.add(weap);
+		}
+		smain.append("]").append("\n");
+	}
     
     
-    public void parseParts(String offset, StringBuilder smain,SquadPartBase part){
-        smain.append(offset).append(part.getId()).append("\n");
-        smain.append(offset).append(part.getUnit().toString()).append("\n");
-        smain.append(offset).append("minsize=").append(part.getMinSize()).append(" maxsize=").append(part.getMaxSize()).append("\n");
+    public void parseParts(String offset, StringBuilder smain,SquadPartBase part,Set<Option> lstopt,Set<Weapon> lstweap,Set<UnitBase> lstunits,Set<ItemBase> lstitems){
+        smain.append(offset).append("unit=").append(part.getId()).append("\n");
+		parseUnits(offset+"\t",smain,part.getUnit(),lstopt,lstweap,lstunits,lstitems);
+		smain.append(offset).append("maxsize=").append(part.getMaxSize()).append(";minsize=").append(part.getMinSize()).append(";unit=").append(id).append("\n");
+		smain.append(offset).append("weaponSelection [").append("\n");
+		for(WeaponSelection wp:part.getWeaponSelection()){
+			
+			smain.append(offset+"\t").append("condition=").append(wp.getCondition()).append(";");
+			smain.append("weaponlist[");
+			for(Weapon w:wp.getWeapon()){
+				lstweap.add(w);
+				smain.append("weapon=").append(w.getWeapon().getId()).append(";");
+			}
+			smain.append("]").append("\n");
+		}
+		smain.append(offset).append("]").append("\n");
         for(SquadPartBase localpart:part.getModifications()){
-            smain.append("[").append("\n");
-            parseParts(offset+"\t",smain,localpart);
-            smain.append("]").append("\n");
+             parseParts(offset+"\t",smain,localpart,lstopt,lstweap,lstunits,lstitems);
         }
     }
     public String marshall(){
         StringBuilder smain=new StringBuilder();
-        Set<Option> lst=new HashSet<Option>();
+        Set<Option> lstopt=new HashSet<Option>();
+		Set<Weapon> lstweap=new HashSet<Weapon>();
+		Set<UnitBase> lstunits=new HashSet<UnitBase>();
+		Set<ItemBase> lstitems=new HashSet<ItemBase>();
         StringBuilder sopt=new StringBuilder(); 
         StringBuilder sweapons=new StringBuilder(); 
         StringBuilder sunits=new StringBuilder(); 
+		StringBuilder sitems=new StringBuilder(); 
         
         smain.append("[\nid=").append(id).append("\n");
         smain.append("name=").append(name).append("\n");
@@ -88,20 +119,21 @@ public class Codex implements java.io.Serializable  {
         String offset="\t";
         for(SquadBase s:squads){
              smain.append(s.getId()).append("\n");
-             smain.append(offset).append(s.getId()).append("\n");
-             smain.append(offset).append(s.getName()).append("\n");
-             smain.append(offset).append(s.getDescription()).append("\n");
-             smain.append(offset).append("options:[").append("\n");
+             smain.append(offset).append("SquadBase=").append(s.getId()).append("\n");
+             smain.append(offset).append("SquadBase name=").append(s.getName()).append("\n");
+             smain.append(offset).append("SquadBase Description=").append(s.getDescription()).append("\n");
+             smain.append(offset).append("options:[");
              offset="\t\t";
              for(Option opt:s.getOptions()){
-                 smain.append(offset).append(opt.getId()).append("\n");
-                 lst.add(opt);
+                 smain.append(opt.getId()).append(";");
+                 lstopt.add(opt);
              }
              offset="\t";
-             smain.append(offset).append("]").append("\n");
-             parseParts(offset,smain,s.getSquadPartBase());
+             smain.append("]").append("\n");
+             parseParts(offset,smain,s.getSquadPartBase(),lstopt,lstweap,lstunits,lstitems);
              
         }
+		smain.append("]");
         return smain.toString();
         
     }
